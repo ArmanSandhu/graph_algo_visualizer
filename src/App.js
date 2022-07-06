5f3423ba0393fac39efdd59bce03efcfbfb301dc
Arman Sandhu <armanssandhu@gmail.com>
2022-07-05 18:08:55 -0700

    Initial Commit - Creating Grid Elements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+const ROWS = 25;
+const COLS = 25; 
 
 function App() {
+
+  const [nodes, setNodes] = useState(Array(ROWS).fill(Array(COLS).fill(null)));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="map">
+      {nodes.map((row, rowIndex) => (
+        <div className="row" id={rowIndex}>
+          {row.map((col, colIndex) => (
+            <div className="node" id={colIndex}></div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
